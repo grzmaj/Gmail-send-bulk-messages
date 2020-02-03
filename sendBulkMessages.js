15b7138296e9da6586a1df6af402bf4316d1ff1b
Grzegorz Maj <37529965+grzmaj@users.noreply.github.com>
2020-02-03 13:04:50 +0100

    Sending random id in every message
    Sends random id in every message instead of number of sent messages (useful when running script several times).

--- sendBulkMessages.js
+++ sendBulkMessages.js
@@ -3,7 +3,9 @@ function sendBulkMessages() {
 	var target_email_address = ""
 	
 	for (n = 0; n < number_of_messages_to_send; ++n) {
-		GmailApp.sendEmail(target_email_address, "Test message #" + n, "Lorem ipsum");
-		Logger.log("Sent message to: " + target_email_address + " subject: " + "Test message #" + n)
+		var random_message_number = Math.floor(Math.random() * 10000)
+		GmailApp.sendEmail(target_email_address, "Test message #" + random_message_number, "Lorem ipsum " + random_message_number);
+		
+		Logger.log("Sent message to: " + target_email_address + " subject: " + "Test message #" + random_message_number)
 	}
 }
